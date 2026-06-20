3f474604348259e9ee7cb96a9bb622134898460a
BurakHINGE <burakmentese16@gmail.com>
2026-06-21 00:49:31 +0300

    Create Input Control and Exit Statue
    Input controls and exit choice have been created.

--- tr_SecondsCalculator.java
+++ tr_SecondsCalculator.java
@@ -4,10 +4,17 @@ public class tr_SecondsCalculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        while (true) {
+        int secim = 1;
+
+        while (secim == 1) {
 
             System.out.print("Saniye değeri giriniz: ");
             int girilenSaniye = input.nextInt();
+
+            while (girilenSaniye < 0) {
+                System.out.print("Lütfen 0'dan büyük bir değer giriniz!\n: ");
+                girilenSaniye = input.nextInt();
+            }
         
             int saat = girilenSaniye / 3600;
             int saattenKalanSaniye = girilenSaniye % 3600;
@@ -34,6 +41,13 @@ public class tr_SecondsCalculator {
 
             System.out.print("ediyor.");
             System.out.println();
+            System.out.print("1-Tekrar Hesapla\n2-Çıkış\n: ");
+            secim = input.nextInt();
+            
+            if (secim == 2) {
+                System.out.println("Sistemden çıkış yapılıyor...");
+                System.exit(0);
+            }
         }
     }    
 }
